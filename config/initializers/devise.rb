f18ddba69ea646803ffdaa583cc029d9ac7ed8f5
jamelablack <jamela.black@gmail.com>
2014-09-28 20:54:58 -0400

    first commit

---
+++ config/initializers/devise.rb
@@ -0,0 +1,7 @@
+# Configuration for Devise specific to your project.
+# BrowserCMS has configured defaults for most other values.
+Devise.setup do |config|
+
+  config.secret_key = 'b8acad045773470a789d55f610b788576ed9e815064ef5448c1ce99290323a53dcb9fbc291dd2f50c612c2222354e48e96824799563fc83da043465dbb3a2497'
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '410f4e018e4fb53230fd750bff6785363a73317d33840f4ec6b752a926321c7effdbc69fc6da8309d72accb295429ae1f083fc46a8ed6d0790999dbfd7f6e7ec'
